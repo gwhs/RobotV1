86709bb98c59c1c8412cb3c933f57fc736fd0447
anton-levin <kyzhao10@gmail.com>
2022-01-19 19:13:31 -0800

    fixing offsets besides back left

--- src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
+++ src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
@@ -26,14 +26,14 @@ public final class SdsModuleConfigurations {
         public static final ModuleConfiguration MK3_FAST_FR = new ModuleConfiguration(
                 0.1016,
                 (16.0 / 48.0) * (28.0 / 16.0) * (15.0 / 60.0), // 6.86 to 1
-                false,
+                true,
                 (15.0 / 32.0) * (10.0 / 60.0),
                 true
         );
         public static final ModuleConfiguration MK3_FAST_BR = new ModuleConfiguration(
                 0.1016,
                 (16.0 / 48.0) * (28.0 / 16.0) * (15.0 / 60.0), // 6.86 to 1
-                false,
+                true,
                 (15.0 / 32.0) * (10.0 / 60.0),
                 true
         );
