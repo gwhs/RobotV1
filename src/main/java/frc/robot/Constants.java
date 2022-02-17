8a9c40687d90fcb4b263d780522b98ef497c059c
Chris <chrsv00ng6@gmail.com>
2022-02-16 18:31:54 -0800

    ADDED UNTESTED PRESETS

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
     public static final int CATAPULT_LEFT_ID = 1;
     public static final int CATAPULT_RIGHT_ID = 21;
     public static double CATAPULT_SPEED = 1.0;
+    public static double CATAPULT_SPEED_LOW = 0.25;
+    public static double CATAPULT_SPEED_DUMP = 0.15;
     public static final int INTAKE_UPPERTALON_ID = 1;
     public static final int INTAKE_LOWERTALON_ID = 1;
     public static final int INTAKE_ALPHANEO_ID = 1;
@@ -17,5 +19,15 @@ public class Constants {
     public static final int followerArm = 23;
     public static final int leaderArm = 32;
 
-
+    public static final int SHOOTER_MODE_DOUBLE = 1;
+    public static final int SHOOTER_MODE_DELAY = 2;
+    public static final int SHOOTER_MODE_LOW_HIGH= 3;
+    public static final int SHOOTER_MODE_DOUBLE_LOW = 4;
+    public static final int SHOOTER_MODE_DUMP = 5;
+    public static final int SHOOTER_MODE_LEFT = 6;
+    public static final int SHOOTER_MODE_RIGHT = 7;
+    public static final int SHOOTER_MODE_LEFT_LOW = 8;
+    public static final int SHOOTER_MODE_RIGHT_LOW =9;
+    public static final int SHOOTER_MODE_LEFT_DUMP = 10;
+    public static final int SHOOTER_MODE_RIGHT_DUMP = 11;
 }
