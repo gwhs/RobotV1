179c69a9c684d3241c9ee16882708e1edf43c0ea
Bennables <bsirival1@gmail.com>
2022-02-16 22:07:43 -0800

    added together climbercommand, extendarm, retractarm to make a file that will do all of it

---
+++ src/main/java/frc/robot/commands/AutoClimbLow.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+import javax.sound.midi.Sequencer;
+
+import edu.wpi.first.math.interpolation.TimeInterpolatableBuffer;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.RetractArm;
+import frc.robot.commands.ExtendArm;
+import frc.robot.commands.ClimberCommand;
+import frc.robot.subsystems.ClimberSubsystem;
+
+public class AutoClimbLow extends SequentialCommandGroup {
+    private ClimberSubsystem climberSubsystem;
+
+    public AutoClimbLow(ClimberSubsystem climberSubsystem){
+        this.climberSubsystem = climberSubsystem;
+    
+    addCommands(new ClimberCommand(climberSubsystem).withTimeout(5), new RetractArm(climberSubsystem).withTimeout(2), new ExtendArm(climberSubsystem));
+    }
+}
