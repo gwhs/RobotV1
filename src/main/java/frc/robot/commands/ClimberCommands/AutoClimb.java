d020cfd8db61ae2ea7f189e5c15cf9ab2a2ee249
Bennables <bensiriv@gmail.com>
2022-02-19 17:45:18 -0800

    ticks work now, ~73k for low, ~272.5k for mid

--- src/main/java/frc/robot/commands/ClimberCommands/AutoClimb.java
+++ src/main/java/frc/robot/commands/ClimberCommands/AutoClimb.java
@@ -14,7 +14,7 @@ public class AutoClimb extends SequentialCommandGroup {
     public AutoClimb(ClimberSubsystem climberSubsystem){
         this.climberSubsystem = climberSubsystem;
 
-        //low ticks - 73k, mid ticks - 272k change this below
+        //low ticks - 73k, mid ticks - 272.5k change this below
     
     addCommands(new ClimberCommand(climberSubsystem, 272500).withTimeout(5), 
     new RetractArm(climberSubsystem));
