45e121e96fcc5f0e067c7d7fb88a6acc9176aae9
Bennables <bensiriv@gmail.com>
2022-02-23 18:28:46 -0800

    added final container, started configuring buttons

--- src/main/java/frc/robot/FinalContainer.java
+++ src/main/java/frc/robot/FinalContainer.java
@@ -11,22 +11,42 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.CatapultSubsystem;
 import frc.robot.subsystems.ClimberSubsystem;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.IntakeMotors;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.commands.ClimberCommands.AutoClimb;
 import frc.robot.commands.ClimberCommands.ClimberCommand;
 import frc.robot.commands.ClimberCommands.ExtendArm;
 import frc.robot.commands.ClimberCommands.RetractArm;
 
 
-public class ClimberContainer {
+public class FinalContainer{
   private final ClimberSubsystem m_ClimberSubsytem = new ClimberSubsystem(43, 45); //FIX INPUTS
+  private final CatapultSubsystem m_LeftCatapultSubsystem = new CatapultSubsystem(14);
+  private final CatapultSubsystem m_RightCatapultSubsystem = new CatapultSubsystem(21);
+  private final IntakeMotors m_IntakeMotors = new IntakeMotors(Constants.INTAKE_UPPERTALON_ID,Constants.INTAKE_LOWERTALON_ID, Constants.INTAKE_ALPHANEO_ID, Constants.INTAKE_BETANEO_ID,Constants.INTAKE_SPEED_TALON1,Constants.INTAKE_SPEED_TALON2, Constants.INTAKE_DEPLOY_SPEED);
   private final XboxController m_controller = new XboxController(0);
+  private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
 
-  public ClimberContainer() {
+  public FinalContainer() {
 
 
-
+    m_drivetrainSubsystem.zeroGyroscope();
+    // Set up the default command for the drivetrain.
+    // The controls are for field-oriented driving:
+    // Left stick Y axis -> forward and backwards movement
+    // Left stick X axis -> left and right movement
+    // Right stick X axis -> rotation
+    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+            m_drivetrainSubsystem,
+            () -> -modifyAxis(m_controller.getLeftY()) * 1, //DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND, //1
+            () -> -modifyAxis(m_controller.getLeftX()) * 1, //DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND, //1
+            //() -> -modifyAxis(m_controller.getRightX()) * 2//DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+            () -> modifyAxis(m_controller.getLeftTriggerAxis() - m_controller.getRightTriggerAxis()) * 2 //DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+    ));
 
     // Configure the button bindings
     configureButtonBindings();
@@ -40,8 +60,18 @@ public class ClimberContainer {
    */
   private void configureButtonBindings() {
     JoystickButton buttonX = new JoystickButton(m_controller, XboxController.Button.kX.value);
-    JoystickButton buttonb = new JoystickButton(m_controller, XboxController.Button.kB.value);
+    JoystickButton buttonB = new JoystickButton(m_controller, XboxController.Button.kB.value);
     JoystickButton buttonA = new JoystickButton(m_controller, XboxController.Button.kA.value);
+    JoystickButton buttonY = new JoystickButton(m_controller, XboxController.Button.kY.value);
+    JoystickButton buttonLTrigger = new JoystickButton(m_controller, XboxController.Button.kLeftBumper.value);
+    JoystickButton buttonRTrigger = new JoystickButton(m_controller, XboxController.Button.kRightBumper.value);
+    JoystickButton button = new JoystickButton(m_controller, XboxController.g.value);
+    JoystickButton button = new JoystickButton(m_controller, XboxController.Button.k.value);
+    JoystickButton button = new JoystickButton(m_controller, XboxController.Button.k.value);
+    JoystickButton button = new JoystickButton(m_controller, XboxController.Button.k.value);
+    JoystickButton button = new JoystickButton(m_controller, XboxController.Button.k.value);
+    JoystickButton button = new JoystickButton(m_controller, XboxController.Button.k.value);
+    JoystickButton button = new JoystickButton(m_controller, XboxController.Button.k.value);
     // Back button zeros the gyroscope
     // new Button(m_controller::getBackButton)
     //         // No requirements because we don't need to interrupt anything
@@ -72,4 +102,23 @@ public class ClimberContainer {
     return new InstantCommand();
   }
 
+  private static double deadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
+  }
+
+  private static double modifyAxis(double value) {
+    // Deadband
+    value = deadband(value, 0.2); //0.05, 0.1 seems to work
+
+
+    return value;
+  }
 }
