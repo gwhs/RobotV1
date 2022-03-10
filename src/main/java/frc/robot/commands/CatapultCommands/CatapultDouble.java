f0adf257b7854c3e022587a86a498a91061de06e
Chris <chrsv00ng6@gmail.com>
2022-03-09 19:49:22 -0800

    wokring catapult code

--- src/main/java/frc/robot/commands/CatapultCommands/CatapultDouble.java
+++ src/main/java/frc/robot/commands/CatapultCommands/CatapultDouble.java
@@ -9,7 +9,7 @@ import frc.robot.subsystems.CatapultSubsystem;
 public class CatapultDouble extends ParallelCommandGroup {
     public CatapultDouble(CatapultSubsystem m_CatapultSubsystemLeft, CatapultSubsystem m_CatapultSubsystemRight, double leftSpeed, double rightSpeed, double delay) {
         addCommands(new CatapultCommand(m_CatapultSubsystemLeft, leftSpeed),
-                    new SequentialCommandGroup(new WaitCommand(delay)),
-                    new CatapultCommand(m_CatapultSubsystemRight, rightSpeed));
+                    new SequentialCommandGroup(new WaitCommand(delay),
+                    new CatapultCommand(m_CatapultSubsystemRight, rightSpeed)));
     } 
 }
