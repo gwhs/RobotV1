bd9bce3342966ee598997bcefbd1458a47241350
anton-levin <kyzhao10@gmail.com>
2022-03-14 16:15:08 -0700

    auto

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -31,15 +31,14 @@ public class AutoCommand extends SequentialCommandGroup {
 
     public AutoCommand(DrivetrainSubsystem m_drivetrainSubsystem, CatapultSubsystem m_catapultSubsystemLeft, CatapultSubsystem m_catapultSubsystemRight, IntakeMotor m_intakeMotor, UpperLowerIntake m_UpperLower) {
         //this.m_drivetrainSubsystem = m_drivetrainSubsystem;
-        PathPlannerTrajectory threeCargoR = PathPlanner.loadPath("2 Cargo - Left", 1, 1);
+        PathPlannerTrajectory threeCargoR = PathPlanner.loadPath("3 Cargo - Right", 1, 1);
         
         PathPlannerTrajectory path = threeCargoR;
         addCommands(
                     new IntakeDeploy(m_intakeMotor, Constants.INTAKE_DEPLOY_SPEED),
                     new CatapultRight(m_catapultSubsystemRight, 0.25),
-                    new InstantCommand(() -> m_drivetrainSubsystem.resetOdometry(new Pose2d(6.89, 4.44, new Rotation2d(Math.toRadians(159.0))))), //left
-                    //new InstantCommand(() -> m_drivetrainSubsystem.resetOdometry(new Pose2d(7.95, 2.73, new Rotation2d(Math.toRadians(-111.00))))), //right
-                    //new InstantCommand(() -> System.out.println(m_drivetrainSubsystem.getPose())),
+                    //new InstantCommand(() -> m_drivetrainSubsystem.resetOdometry(new Pose2d(6.89, 4.44, new Rotation2d(Math.toRadians(159.0))))), //left
+                    new InstantCommand(() -> m_drivetrainSubsystem.resetOdometry(new Pose2d(7.95, 2.73, new Rotation2d(Math.toRadians(-111.00))))), //right
                     new ParallelCommandGroup(
                          new SpinIntake(m_UpperLower, Constants.INTAKE_UPPER_SPEED, Constants.INTAKE_LOWER_SPEED).withTimeout(path.getTotalTimeSeconds()),
                          new PPSwerveControllerCommand(
