bb70c9abb86eb94758f21582f56659be987a50ef
Chris <chrsv00ng6@gmail.com>
2022-03-14 18:19:25 -0700

    added command for increase method

--- src/main/java/frc/robot/CatapultContainer.java
+++ src/main/java/frc/robot/CatapultContainer.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.CatapultCommands.CatapultDouble;
 import frc.robot.commands.CatapultCommands.CatapultLeft;
 import frc.robot.commands.CatapultCommands.CatapultRight;
+import frc.robot.commands.CatapultCommands.IncreaseCatapultPower;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -17,6 +18,7 @@ public class CatapultContainer implements BaseContainer{
     private final XboxController m_controller = new XboxController(0);
     private final CatapultSubsystem m_CatapultSubsystemLeft = new CatapultSubsystem(Constants.CATAPULT_LEFT_ID, false);
     private final CatapultSubsystem m_CatapultSubsystemRight = new CatapultSubsystem(Constants.CATAPULT_RIGHT_ID, true);
+    private CatapultSubsystem m_CatapultSubsystem;
 
     public CatapultContainer(){
 
@@ -27,6 +29,7 @@ public class CatapultContainer implements BaseContainer{
         JoystickButton buttonB = new JoystickButton(m_controller, XboxController.Button.kB.value);
         JoystickButton buttonX = new JoystickButton(m_controller, XboxController.Button.kX.value);
         JoystickButton buttonA = new JoystickButton(m_controller, XboxController.Button.kA.value);
+        JoystickButton buttonRB = new JoystickButton(m_controller, XboxController.Button.kRightBumper.value);
         // SpinMotor s
         // change motor and speed here. SpinMotor(motor id, percent output[-1 to 1 as double])
         // buttonB.whenPressed( new CatapultDouble(m_CatapultSubsystemLeft,m_CatapultSubsystemRight, Constants.SHOOTER_MODE_LEFT));
@@ -36,7 +39,7 @@ public class CatapultContainer implements BaseContainer{
         buttonB.whenPressed(new CatapultLeft(m_CatapultSubsystemLeft, Constants.CATAPULT_LEFT_SPEED));
         buttonX.whenPressed(new CatapultRight(m_CatapultSubsystemRight, Constants.CATAPULT_RIGHT_SPEED));
         buttonA.whenPressed(new CatapultDouble(m_CatapultSubsystemLeft, m_CatapultSubsystemRight, Constants.CATAPULT_LEFT_SPEED, Constants.CATAPULT_RIGHT_SPEED, Constants.CATAPULT_DELAY));
-        
+        buttonRB.whenPressed(new IncreaseCatapultPower(m_CatapultSubsystem, 1));
 
     }
 
